39e4b6476a52492b7f0f59cd7cf8b82d0cbeed03
thais de rodez <thaisderodez@gmail.com>
2022-08-12 16:53:13 +0200

    Feat(Api): created Api class for photograh & media

---
+++ scripts/api/Api.js
@@ -0,0 +1,35 @@
+class Api {
+  constructor(url) {
+    this._url = url;
+  }
+
+  async get() {
+    try {
+      const res = await fetch(this._url);
+      const data = await res.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+class PhotographerApi extends Api {
+  constructor(url) {
+    super(url);
+  }
+
+  async getPhotographers() {
+    return await this.get().photographers;
+  }
+}
+
+class MediaApi extends Api {
+  constructor(url) {
+    super(url);
+  }
+
+  async getMedias() {
+    return await this.get().medias;
+  }
+}
