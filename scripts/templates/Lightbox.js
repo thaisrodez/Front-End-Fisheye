ee3c39fbebd6b060b935a1c7d366461feb31b6bd
thais de rodez <thaisderodez@gmail.com>
2022-08-26 13:41:28 +0200

    Feat(lightbox): media and navigation display

--- scripts/templates/Lightbox.js
+++ scripts/templates/Lightbox.js
@@ -3,6 +3,7 @@ class LightBox {
     this._image = media._image;
     this._video = media._video;
     this._title = media._title;
+    this._mediaPath = media.getMediaPath();
 
     // lightbox modal inner div
     this.$lightbox = document.createElement("div");
@@ -21,22 +22,55 @@ class LightBox {
   }
 
   createLightbox() {
+    // empty lightbox
+    this.$lightboxModal.innerHTML = "";
     // create DOM element
-    const closeButton = document.createElement("img");
+    const leftDiv = document.createElement("div");
+    const middleDiv = document.createElement("div");
+    const rightDiv = document.createElement("div");
+    const closeButton = document.createElement("button");
     let mediaElement = null;
     this._image
       ? (mediaElement = document.createElement("img"))
       : (mediaElement = document.createElement("video"));
+    const mediaTitle = document.createElement("p");
+    const previous = document.createElement("a");
+    const next = document.createElement("a");
 
     // set attributes
-    closeButton.setAttribute("src", "assets/icons/close.svg");
-    closeButton.setAttribute("role", "button");
-    closeButton.setAttribute("tabindex", "0");
     closeButton.setAttribute("id", "close_lightbox");
-    closeButton.setAttribute("alt", "Fermer le formulaire de contact");
+    closeButton.setAttribute("aria-label", "Fermer le formulaire de contact");
 
-    // insert element in DOM
-    this.$lightbox.appendChild(closeButton);
+    mediaElement.setAttribute("src", this._mediaPath);
+    this._image
+      ? mediaElement.setAttribute("alt", this._title)
+      : mediaElement.setAttribute("controls", "");
+
+    previous.setAttribute("aria-label", "image suivante");
+    next.setAttribute("aria-label", "image précédente");
+
+    // add classes
+    mediaElement.classList.add("lightbox-media");
+    mediaTitle.classList.add("red-text");
+    closeButton.classList.add("red-text", "lightbox-navigation");
+    previous.classList.add("red-text", "lightbox-navigation");
+    next.classList.add("red-text", "lightbox-navigation");
+
+    // add content
+    mediaTitle.textContent = this._title;
+    closeButton.innerHTML = "<i class='fa-solid fa-xmark'></i>";
+    previous.innerHTML = "<i class='fa-solid fa-chevron-left'></i>";
+    next.innerHTML = "<i class='fa-solid fa-chevron-right'></i>";
+
+    // insert element in DOM$
+    leftDiv.appendChild(previous);
+    this.$lightbox.appendChild(previous);
+    middleDiv.appendChild(mediaElement);
+    middleDiv.appendChild(mediaTitle);
+    this.$lightbox.appendChild(middleDiv);
+    rightDiv.appendChild(closeButton);
+    rightDiv.appendChild(next);
+    this.$lightbox.appendChild(rightDiv);
     this.$lightboxModal.appendChild(this.$lightbox);
 
     // display modal
