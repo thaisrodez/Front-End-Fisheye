a62012753870e89d047bff7adca24d7a2eb93d98
thais de rodez <thaisderodez@gmail.com>
2022-09-22 12:11:57 +0200

    Fix(photo page): refactoring

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -72,38 +72,30 @@ class PhotographerPage {
     const medias = await this.getPhotographerMedias();
     let totalLikesArray = [];
 
+    // insert sorting button
+    const Sorter = new SorterForm(medias, currentPhotographer);
+    Sorter.render();
+
+    const handleLightBox = new HandleLightbox(medias, currentPhotographer);
+
     // insert photographer medias
     medias.forEach((media) => {
       totalLikesArray.push(media.likes);
       const mediaModel = new Media(media, currentPhotographer);
       const mediaDom = mediaModel.getMediaCardDom();
       this.$portfolio.appendChild(mediaDom);
-    });
 
-    // get total Likes
-    const totalLikes = totalLikesArray.reduce((a, b) => a + b);
-
-    // insert photographer bio details
-    const photographerModel = new Photographer(currentPhotographer, totalLikes);
-
-    const { nameDiv, img, insert } =
-      photographerModel.getPhotographerHeaderDOM();
-    this.$photographerHeader.prepend(nameDiv);
-    this.$photographerHeader.appendChild(img);
-    this.$photographerMain.appendChild(insert);
-
-    // handle likes
-    this.handleLikes();
-
-    const handleLightBox = new HandleLightbox(medias, currentPhotographer);
-    document.querySelectorAll(".media-object").forEach((mediaDom) => {
-      mediaDom.addEventListener("click", (e) => {
+      // display lightbox
+      const imgElement = mediaDom.firstChild;
+      // click
+      imgElement.addEventListener("click", (e) => {
         // get element throught dataset
         handleLightBox.show(e.target.dataset.id);
         // set focus on lightbox navigation
         document.getElementById("previous-media").focus();
       });
-      mediaDom.addEventListener("keydown", (e) => {
+      // keyboard
+      imgElement.addEventListener("keydown", (e) => {
         if (
           this.$lightboxModal.getAttribute("aria-hidden") === "true" &&
           e.key === "Enter"
@@ -114,9 +106,19 @@ class PhotographerPage {
       });
     });
 
-    // insert sorting button
-    const Sorter = new SorterForm(medias, currentPhotographer);
-    Sorter.render();
+    // get total Likes
+    const totalLikes = totalLikesArray.reduce((a, b) => a + b);
+    // handle likes
+    this.handleLikes();
+
+    // insert photographer bio details
+    const photographerModel = new Photographer(currentPhotographer, totalLikes);
+
+    const { nameDiv, img, insert } =
+      photographerModel.getPhotographerHeaderDOM();
+    this.$photographerHeader.prepend(nameDiv);
+    this.$photographerHeader.appendChild(img);
+    this.$photographerMain.appendChild(insert);
 
     // insert contact form
     const Form = new ContactForm(currentPhotographer);
